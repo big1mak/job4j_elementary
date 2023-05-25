330fc79b377a47d494f503492c8c87bcb97b97d5
big1mak <kovalenko_nick@mail.ru>
2023-05-25 13:13:19 +0300

    9.1. String [#237407]

---
+++ src/main/java/ru/job4j/condition/Greeting.java
@@ -0,0 +1,12 @@
+package ru.job4j.condition;
+
+public class Greeting {
+    public static void main(String[] arga) {
+        String idea = "I like Java!";
+        System.out.println(idea);
+        idea += "But i am a newbie.";
+        int yaer = 2023;
+        idea += yaer;
+        System.out.println(idea);
+    }
+}
